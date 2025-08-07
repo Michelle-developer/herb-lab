55fa68c5b9cf4b0c6a7e5959fd09025b91a8baf4
Michelle <miiitiiigg@gmail.com>
2025-08-07 13:37:29 +0800

    remove axiosRetry

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import axiosRetry from 'axios-retry';
 import { HerbProvider } from './contexts/HerbContext.jsx';
 import { ConstitutionProvider } from './contexts/ConstitutionContext.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
